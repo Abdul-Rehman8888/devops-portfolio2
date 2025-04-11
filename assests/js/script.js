62273d95ff97f5191696c23f96ea72dd4f558d95
Abdul-Rehman8888 <156101553+Abdul-Rehman8888@users.noreply.github.com>
2025-04-11 20:34:36 +0530

    Update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -53,7 +53,7 @@ function(){
 
 // <!-- typed js effect starts -->
     var typed = new Typed(".typing-text", {
-        strings: ["DevOps Engineer","Full-Stack development", "Cloud Computing"],
+        strings: ["DevOps Engineering", "Cloud Engineering", "Platform Engineering"],
         loop: true,
         typeSpeed: 50,
 		backSpeed: 25,
