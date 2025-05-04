f635fee9fd0527872f853b0409e05b9e5e7c653b
Abdul-Rehman8888 <156101553+Abdul-Rehman8888@users.noreply.github.com>
2025-05-04 20:14:54 +0530

    Update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -71,14 +71,14 @@ function(){
 
 // Start of Tawk.to Live Chat
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-  (function(){
-  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-  s1.async=true;
-  s1.src='https://embed.tawk.to/666ae61c981b6c56477cbf08/1i08p5oeo';
-  s1.charset='UTF-8';
-  s1.setAttribute('crossorigin','*');
-  s0.parentNode.insertBefore(s1,s0);
-  })();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/68177bef102ecb191265d241/1iqdro95i';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
 // End of Tawk.to Live Chat
 
  // <!-- emailjs to mail contact form data -->
