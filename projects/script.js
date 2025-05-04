4e9dde9d35ddf68697d4bd0225adc7f4e56eb89d
Abdul-Rehman8888 <156101553+Abdul-Rehman8888@users.noreply.github.com>
2025-05-04 20:24:42 +0530

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -87,15 +87,8 @@ getProjects().then(data => {
 // fetch projects end
 
 // Start of Tawk.to Live Chat
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/68177bef102ecb191265d241/1iqdro95i';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+
+
 // End of Tawk.to Live Chat
 
 // disable developer mode
